9babcd95e8831998d8413b46baff1deeb2fad0fe
TheHuntsman4 <anikethvij464@gmail.com>
2023-01-24 21:42:06 +0530

    started with home page

---
+++ mysite/blog/views.py
@@ -0,0 +1,13 @@
+
+from django.shortcuts import render
+
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def home(request):
+    return render(request,'blog/home.html')
+
