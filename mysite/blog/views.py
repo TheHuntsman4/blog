bdae92ee45d52a0782e708728720293268ed23ac
TheHuntsman4 <anikethvij464@gmail.com>
2023-01-25 18:30:34 +0530

    Add Post model and home post page view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,20 @@
 
-from django.shortcuts import render
 
+from django.template import loader
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-
+from .models import Post
 from django.shortcuts import redirect
-
+from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'blog/home.html')
+    posts=Post.objects.all().values()
+    template=loader.get_template("blog/home.html")
+    context={
+        'posts':posts,
+    }
+
+    
+    return HttpResponse(template.render(context,request))
 
